db7f209741c65e5a4993a6a4663b7f4f0bd9e72e
Vivek Jain <vivekjain10@gmail.com>
2014-01-25 15:29:07 -0500

    Lock/Unlock based on RSSI

--- Arduino/RFduinoProximityLock/RFduinoProximityLock.ino
+++ Arduino/RFduinoProximityLock/RFduinoProximityLock.ino
@@ -1,9 +1,14 @@
+#include <Servo.h>
 #include <RFduinoBLE.h>
 
 const char *lockId = "0D624DE";
 const char *validKey = "34C";
+boolean validKeyInRange = false;
+int servoPin = 6;
+Servo s1;
 
 void setup() {
+  s1.attach(servoPin);
   Serial.begin(9600);
   RFduinoBLE.deviceName = "BLELock";
   RFduinoBLE.advertisementInterval = 100; //100ms 
@@ -26,7 +31,15 @@ void RFduinoBLE_onAdvertisement(bool start)
 //returns the dBm signal strength after connecting 
 void RFduinoBLE_onRSSI(int rssi)
 {
-//  Serial.println(rssi);
+  if(validKeyInRange)
+  {
+    Serial.println(rssi);
+    if(rssi > -50)
+      s1.write(0); //Unlock
+    else if(rssi < -60)
+      s1.write(90); //Lock
+    //Don't change lock state when rssi from -50 to -60
+  }
 }
 
 //data from the radio
@@ -34,8 +47,21 @@ void RFduinoBLE_onReceive(char *key, int len)
 {
   Serial.println(key);
   if(!strcmp(key, validKey))
-    Serial.println("un-lock");
+  {
+    Serial.println("valid key");
+    validKeyInRange = true;
+  }
   else
-    Serial.println("lock");
-} 
+  {
+    Serial.println("invalid key");
+    validKeyInRange = false;
+  }
+}
 
+void RFduinoBLE_onConnect(){
+  Serial.println("On Connect");
+}
+
+void RFduinoBLE_onDisconnect(){
+  Serial.println("On Disconnect");
+}
